d6495cb7edbafac8c3d089401a15fcda284a2cd8
vinayak-chavan <72243768+vinayak-chavan@users.noreply.github.com>
2023-04-21 11:52:55 +0530

    Add files via upload

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Footer = () => { 
+  return (
+    <div>
+      <footer className="page-footer navbar-light bg-light fixed-bottom">
+        <div className="footer-copyright text-center py-3">
+          © 2023 Copyright:
+          <a href="/"> oneclick.com</a>
+        </div>
+      </footer>
+    </div>
+  );
+}
